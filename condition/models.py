e5e2166c29d87ebe077823fce92649f42b2b4e4c
Maryam Jafari <jafarimaryam2002@gmail.com>
2022-08-18 13:08:50 +0430

    coditionmodel

--- condition/models.py
+++ condition/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Condition(models.Model):
+    condiotion = models.CharField(max_length=255, primary_key=True)
+    id = models.IntegerField(unique=True)
+
+    def __str__(self) -> str:
+        return ' '.join(self.condiotion.split('-')) 
+
+
