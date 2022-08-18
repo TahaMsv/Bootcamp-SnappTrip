6cc795b9846a8d618a026b47cef908a65a0daa93
AmirHossein <khanzadeh78ah@gmail.com>
2022-08-18 17:09:59 +0430

    :bug: Fixed typo

--- condition/models.py
+++ condition/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Condition(models.Model):
-    condiotion = models.CharField(max_length=255, primary_key=True)
+    condition = models.CharField(max_length=255, primary_key=True)
     id = models.IntegerField(unique=True)
     name= models.CharField(max_length=255)
     operator= models.CharField(max_length=7)
