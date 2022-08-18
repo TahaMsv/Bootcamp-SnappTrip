642fe25fc26498995aabb77fd809871e7a6adf77
AmirHossein <khanzadeh78ah@gmail.com>
2022-08-18 13:30:05 +0430

    :sparkles: Added ruleaction api structure

---
+++ ruleaction/api/views.py
@@ -0,0 +1,12 @@
+from condition.models import Condition
+from ruleaction.models import Rule
+
+
+# def check_contion(request)
+#
+#     a = {
+#         "age": 50,
+#         "usertype": "b2b"
+#     }
+#
+#     Condition.objects.filter(name=name, valie= )
